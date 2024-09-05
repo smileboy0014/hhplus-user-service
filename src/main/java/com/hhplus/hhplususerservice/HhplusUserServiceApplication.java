b48464ff4c1810d2cafa9c0d8b36a3c58adeac18
gt.park <smileboy0014@gmail.com>
2024-09-05 16:38:15 +0900

    first commit

---
+++ src/main/java/com/hhplus/hhplususerservice/HhplusUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hhplus.hhplususerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhplusUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hhplus/hhplususerservice/HhplusUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.hhplususerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
